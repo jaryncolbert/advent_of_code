1f958a10ac78516d313816092bd2265331ac0a8d
Jaryn Colbert <jaryn.colbert@greenhouse.io>
2021-12-03 01:02:20 -0500

    Correct oxygen, incorrect CO2

--- day_3/power_consumption.rb
+++ day_3/power_consumption.rb
@@ -26,49 +26,36 @@ def consumption(binary_lines)
   puts "Bit String: #{bit_string}, Bin: #{gamma}, inverse: #{epsilon}, Consumption: #{gamma * epsilon}"
 end
 
-def life_support_rating(binary_lines)
-  bit_hash = {}
-
-  binary_lines.each do |line|
-    line.split('').each_with_index do |char, index|
-      if bit_hash[index].nil?
-        bit_hash[index] = [
-          { value: 0, count: 0, binary_nums: [] },
-          { value: 1, count: 0, binary_nums: [] }
-        ]
-      end
-
-      if char == '0'
-        bit_hash[index][0][:count] += 1
-        bit_hash[index][0][:binary_nums] << line
-      else
-        bit_hash[index][1][:count] += 1
-        bit_hash[index][1][:binary_nums] << line
-      end
-    end
-  end
-
-  curr_index = 0
-  loop do
-    relevant_values = bit_hash[curr_index].map do |index0, index1|
-      index0[:count] > index1[:count] ? index0[:binary_nums] : index1[:binary_nums]
-    end.flatten
-
-    break if relevant_values.length == 1
-
-    curr_index += 1
-  end
-
-
-  # puts "Bit String: #{bit_string}, Bin: #{gamma}, inverse: #{epsilon}, Consumption: #{gamma * epsilon}"
-  puts "Bit Hash: #{bit_hash}"
-end
-
 def ones_complement(input_string)
   input_string.to_i(2) ^ ((1 << input_string.length) - 1)
 end
 
-binary_lines = File.readlines("test.txt").map(&:chomp)
+def oxygen_generator(binary_lines)
+  curr_index = 0
+  filtered_lines = binary_lines
+
+  loop do
+    one_bit_nums = []
+    zero_bit_nums = []
+
+    filtered_lines.each do |line|
+      curr_char = line[curr_index]
+      curr_char == '0' ? zero_bit_nums << line : one_bit_nums << line
+    end
+
+    relevant_nums = one_bit_nums.length >= zero_bit_nums.length ? one_bit_nums : zero_bit_nums
+
+    return relevant_nums.first if relevant_nums.length == 1
+
+    filtered_lines = relevant_nums
+    curr_index += 1
+  end
+end
+
+binary_lines = File.readlines("input.txt").map(&:chomp)
 #consumption(binary_lines)
-life_support_rating(binary_lines)
+oxygen_generator = life_support_rating(binary_lines)
+co2_scrubber = ones_complement(oxygen_generator)
+puts "Oxygen: #{oxygen_generator}, CO2: #{"100001111000"}"
+puts "Oxygen: #{oxygen_generator.to_i(2)}, CO2: #{"100001111000".to_i(2)}, Multiplied: #{oxygen_generator.to_i(2) * co2_scrubber}"
 
