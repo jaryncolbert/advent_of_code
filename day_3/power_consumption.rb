7862e9d348099f9380b2ea85a1e905c3eb359b61
Jaryn Colbert <jaryn.colbert@greenhouse.io>
2021-12-03 01:08:57 -0500

    Day 3, Part 2: Done

--- day_3/power_consumption.rb
+++ day_3/power_consumption.rb
@@ -30,32 +30,40 @@ def ones_complement(input_string)
   input_string.to_i(2) ^ ((1 << input_string.length) - 1)
 end
 
-def oxygen_generator(binary_lines)
+def process_binary_nums(binary_lines, keep_largest: true)
   curr_index = 0
-  filtered_lines = binary_lines
+  filtered_nums = binary_lines
 
   loop do
     one_bit_nums = []
     zero_bit_nums = []
 
-    filtered_lines.each do |line|
+    filtered_nums.each do |line|
       curr_char = line[curr_index]
       curr_char == '0' ? zero_bit_nums << line : one_bit_nums << line
     end
 
-    relevant_nums = one_bit_nums.length >= zero_bit_nums.length ? one_bit_nums : zero_bit_nums
+    relevant_nums = if (keep_largest && one_bit_nums.length >= zero_bit_nums.length) ||
+                       (!keep_largest && one_bit_nums.length < zero_bit_nums.length)
+                      one_bit_nums
+                    else
+                      zero_bit_nums
+                    end
 
     return relevant_nums.first if relevant_nums.length == 1
 
-    filtered_lines = relevant_nums
+    filtered_nums = relevant_nums
     curr_index += 1
   end
 end
 
 binary_lines = File.readlines("input.txt").map(&:chomp)
 #consumption(binary_lines)
-oxygen_generator = life_support_rating(binary_lines)
-co2_scrubber = ones_complement(oxygen_generator)
-puts "Oxygen: #{oxygen_generator}, CO2: #{"100001111000"}"
-puts "Oxygen: #{oxygen_generator.to_i(2)}, CO2: #{"100001111000".to_i(2)}, Multiplied: #{oxygen_generator.to_i(2) * co2_scrubber}"
+oxygen_generator = process_binary_nums(binary_lines)
+co2_scrubber = process_binary_nums(binary_lines, keep_largest: false)
+puts "Oxygen Bin: #{oxygen_generator}, CO2 Bin: #{co2_scrubber}"
+
+oxygen_generator_dec = oxygen_generator.to_i(2)
+co2_scrubber_dec = co2_scrubber.to_i(2)
+puts "Oxygen Dec: #{oxygen_generator_dec}, CO2 Dec: #{co2_scrubber_dec}, Multiplied: #{oxygen_generator_dec * co2_scrubber_dec}"
 
