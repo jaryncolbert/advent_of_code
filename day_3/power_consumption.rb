c0132b2c81ea5750fd676512a401ef2fc454db26
Jaryn Colbert <jaryn.colbert@greenhouse.io>
2021-12-03 00:42:52 -0500

    Rabbit hole solving wrong problem

--- day_3/power_consumption.rb
+++ day_3/power_consumption.rb
@@ -26,10 +26,49 @@ def consumption(binary_lines)
   puts "Bit String: #{bit_string}, Bin: #{gamma}, inverse: #{epsilon}, Consumption: #{gamma * epsilon}"
 end
 
+def life_support_rating(binary_lines)
+  bit_hash = {}
+
+  binary_lines.each do |line|
+    line.split('').each_with_index do |char, index|
+      if bit_hash[index].nil?
+        bit_hash[index] = [
+          { value: 0, count: 0, binary_nums: [] },
+          { value: 1, count: 0, binary_nums: [] }
+        ]
+      end
+
+      if char == '0'
+        bit_hash[index][0][:count] += 1
+        bit_hash[index][0][:binary_nums] << line
+      else
+        bit_hash[index][1][:count] += 1
+        bit_hash[index][1][:binary_nums] << line
+      end
+    end
+  end
+
+  curr_index = 0
+  loop do
+    relevant_values = bit_hash[curr_index].map do |index0, index1|
+      index0[:count] > index1[:count] ? index0[:binary_nums] : index1[:binary_nums]
+    end.flatten
+
+    break if relevant_values.length == 1
+
+    curr_index += 1
+  end
+
+
+  # puts "Bit String: #{bit_string}, Bin: #{gamma}, inverse: #{epsilon}, Consumption: #{gamma * epsilon}"
+  puts "Bit Hash: #{bit_hash}"
+end
+
 def ones_complement(input_string)
   input_string.to_i(2) ^ ((1 << input_string.length) - 1)
 end
 
 binary_lines = File.readlines("test.txt").map(&:chomp)
-consumption(binary_lines)
+#consumption(binary_lines)
+life_support_rating(binary_lines)
 
